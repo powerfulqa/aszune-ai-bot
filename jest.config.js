ed3c1b102dd5229652f4ef8de8995367bfbf1771
Chris Haycock <ch@Pascal-audio.com>
2025-05-26 17:20:53 +0200

    Update Jest config to use v8 coverage provider and exclude non-source files from coverage

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,10 @@ module.exports = {
     "**/*.js",
     "!**/node_modules/**",
     "!**/__tests__/**",
-    "!**/coverage/**"
-  ]
+    "!**/coverage/**",
+    "!ecosystem.config.js", // Exclude config files that are not code
+    "!index copy.js"        // Exclude backup or unused files
+  ],
+  // Add this to ensure coverage is reported for files not required in tests
+  coverageProvider: "v8"
 };
