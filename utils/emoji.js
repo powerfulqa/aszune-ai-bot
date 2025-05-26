f3c50e6424ac0ae42ce50afd2749ada1295edf83
Chris Haycock <ch@Pascal-audio.com>
2025-05-26 17:02:36 +0200

    Update for Tests

--- utils/emoji.js
+++ utils/emoji.js
@@ -13,7 +13,9 @@ const emojiMap = {
 function appendEmoji(text) {
   let result = text;
   for (const [keyword, emoji] of Object.entries(emojiMap)) {
-    if (text.toLowerCase().includes(keyword)) {
+    // Only match whole words (case-insensitive)
+    const regex = new RegExp(`\\b${keyword}\\b`, 'i');
+    if (regex.test(text)) {
       result += ` ${emoji}`;
     }
   }
