10c5e95d78bccaad1b2f394111599c9b223e0141
Chris Haycock <chrishaycock@hotmail.com>
2025-10-03 22:52:28 +0200

    fix: Use correct Perplexity model name llama-3.1-sonar-small-128k-chat

--- src/config/config.js
+++ src/config/config.js
@@ -140,7 +140,7 @@ const config = {
       ENDPOINTS: {
         CHAT_COMPLETIONS: '/chat/completions',
       },
-      DEFAULT_MODEL: 'llama-3.1-sonar-large-128k-online',
+      DEFAULT_MODEL: 'llama-3.1-sonar-small-128k-chat',
       DEFAULT_TEMPERATURE: 0.0,
       MAX_TOKENS: {
         CHAT: 1024,
